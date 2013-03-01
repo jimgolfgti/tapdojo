eeeedda195fe7e959fe77c7c056839eaa388d35d
jimgolfgti <jimgolfgti@gmail.com>
2013-03-01 00:20:31 +0000

    CopyToAsync exercise

---
+++ source/CopyToAsync/CopyToAsync/StreamExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CopyToAsync
+{
+    public static class StreamExtensions
+    {
+        public static async Task CopyToAsync(this Stream source, Stream destination,
+                                             CancellationToken cancellationToken,
+                                             IProgress<long> progress)
+        {
+        }
+    }
+}
